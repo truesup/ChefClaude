7c6a0ef0a0596f2457f70fbae5b95af7da103200
Pavel Merekin <merekin33@gmail.com>
2025-03-18 14:31:11 +0700

    Main element and form added and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import Main from './components/Main'
 
 export default function App() {
   return (
     <div className="app-wrapper">
       <Header />
+      <Main />
     </div>
   )
 }
